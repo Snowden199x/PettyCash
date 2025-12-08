40b5ef38210c8bd572cc714b666e131e6ea023de
Snowden199x <goco.pj.bsinfotech@gmail.com>
2025-12-08 10:43:35 +0800

    FIXED

--- mobile_application/lib/api_client.dart
+++ mobile_application/lib/api_client.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiClient {
-  static const String baseUrl = 'http://192.168.1.12:5000'; // Replace with your server's address or IP for device access
+  static const String baseUrl = 'http://10.216.33.26:5000'; // Replace with your server's address or IP for device access
 
   Future<Map<String, dynamic>> postJson(
     String path,
